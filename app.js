0ee687edc44cbdb2d750d3cd1cc103a91d33a106
sleklere <santiagoleklere@gmail.com>
2022-09-26 20:08:12 -0300

    view registro creada + estructura y estilo basico

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
 app.use(express.static("public"));
